750f39adea214e8f71c290c0256141c5ad01dbc6
c-hudson <cmhudson@gmail.com>
2021-10-08 21:04:09 -0500

    Add files via upload

--- has.pl
+++ has.pl
@@ -1,4 +1,35 @@
 #!/usr/bin/perl
+############################################################################
+#                                                                          #
+# has.pl - Higher Availability Service                                     #
+#                                                                          #
+#    This service is a front end to a TinyMUSH like server to help         #
+#    minimize down times that the user sees and has to react to.           #
+#    This means if the MUSH goes down runs on goes down, the connection    #
+#    between the user and the has service will be kept open. Once the      #
+#    MUSH service is restored, the user will be re-connected to the MUSH   #
+#    and the user will be logged in.                                       #
+#                                                                          #
+#    This means you can be free to crash your MUSH, restart your server,   #
+#    install upates, have power outages, or let your cat turn your server  #
+#    off by steping on the power button as long as you restart your        #
+#    MUSH server. For this to happen, your has server has to be running    #
+#    on a different machine. Preferably something with a battery or        #
+#    in a different location.                                              #
+#                                                                          #
+#    The has server also supports "failing over" to another server.        #
+#    Currently this is supported by manually changing the address          #
+#    and sending the process a HUP signal. Version 2 of the script will    #
+#    support automatic failover after a timeout period. Keep in mind that  #
+#    the fail over database needs to have the users already created        #
+#    for this to work or they will not be auto logged in.                  #
+#                                                                          #
+#    Configuration - See the init_data subroutine. A config file will      #
+#    be used in the future. Wait for version 2.0 as more important         #
+#    stability issues are being addressed at this time.                    #
+#                                                                          #
+############################################################################
+
 use strict;
 use IO::Select;
 use IO::Socket;
@@ -9,9 +40,67 @@ my (%conn,                                                 # connection data
     $listener,                                                # the listener
     $readable,                                            # sockets to watch
     $hb,                                     # heartbeat connection to world
-    @que,                                       # input que to watch out for
     %data,                          # misc data not specific to a connection
    );
+
+#
+# init_data
+#    Load the data variable with any default values as needed. If the
+#    service is sent a HUP signal, this data and all code will be reloaded
+#    and used.
+#
+sub init_data
+{
+   # keep track for fail over purposes, no need to change this.
+   if(@data{prev_mush_address} ne @data{mush_address}) {
+      @data{prev_mush_address} = @data{mush_address};
+   }
+
+   #--- [ start user configuration section ]-------------------------------#
+
+   # regexp string to find if the user successfully connected or not.
+   @data{connect_fail} = "Either that player .*not exist.*";
+   @data{connect_success} = "Last connect was from.*";
+
+   # address of the actual mush
+   @data{mush_address} = "192.168.1.7:4096";
+
+   # port that this service will listen on
+   @data{local_port} = 4000;
+
+   # how often to try to reconnect
+   @data{beat} = 10;
+
+   # credentials for the hearbeat user
+   @data{hb_user} = "heartbeat";
+   @data{hb_pass} = "heartbeat";
+
+   # Command to send to the mush to register the client's real hostname
+   # Setting this variable to an empty string will cause it not to send
+   # the command.
+   @data{remotehostname_cmd} = "\@REMOTEHOSTNAME";
+
+   # message shown when the world goes offline
+   @data{offline_notice} = <<'   __EOF__';
+   # NOTICE # This world is OFFLINE. Please wait till it returns.
+   __EOF__
+
+   # message shown when the world goes online
+   @data{online_notice}=<<'   __EOF__';
+   # NOTICE # This world is ONLINE. Normality has returned.
+   __EOF__
+
+
+   #--- [ end user configuration section ]-------------------------------#
+   @data{offline_notice} =~ s/^   //gm;
+   @data{online_notice} =~ s/^   //gm;
+   @data{offline_notice} =~ s/([\r\n]+)$//;
+   @data{online_notice} =~ s/([\r\n]+)$//;
+
+   if(@data{prev_mush_address} ne @data{mush_address}) {
+      printf("Remote Address: %s\n",@data{mush_address});
+   }
+}
 
 #
 # code
@@ -35,43 +124,22 @@ sub code
    }
 }
 
+#
+# offline
+#    is the hb user online?
+#
 sub offline
 {
    return ($hb eq undef) ? 1 : 0;
 }
 
 #
-# init_data
-#    initalize any data that can be reloaded as needed.
+# offline
+#    is the hb user offline?
 #
-sub init_data
+sub online
 {
-   # keep track for fail over purposes
-   if(@data{prev_mush_address} ne @data{mush_address}) {
-      @data{prev_mush_address} = @data{mush_address};
-   }
-
-   @data{mush_address} = "192.168.1.7:4096";
-   @data{local_port} = 4096;
-   @data{beat} = 10;
-   @data{timeout} = 2;
-   @data{hb_user} = "heartbeat";
-   @data{hb_pass} = "heartbeat";
-   @data{offline_notice} = <<'   __EOF__';
-   # NOTICE # This world is OFFLINE. Please wait till it returns.
-   __EOF__
-   @data{online_notice}=<<'   __EOF__';
-   # NOTICE # This world is ONLINE. Normality has returned.
-   __EOF__
-
-   @data{offline_notice} =~ s/^   //gm;
-   @data{online_notice} =~ s/^   //gm;
-   @data{offline_notice} =~ s/([\r\n]+)$//;
-   @data{online_notice} =~ s/([\r\n]+)$//;
-
-   if(@data{prev_mush_address} ne @data{mush_address}) {
-      printf("Remote Address: %s\n",@data{mush_address});
-   }
+   return ($hb eq undef) ? 0 : 1;
 }
 
 
@@ -95,38 +163,42 @@ sub handle_disconnect
 
          @conn{$s}->{disconnect} = time();
    
-         if($hb eq undef) {
+         if($hb eq undef) {                  # hb is offline, world is down
             disconnect_world($s);
             @conn{$s}->{reconnect} = 1;
          } else {
+            #
+            # send a command to the HB user. If we get the responce, the 
+            # server is up and the client should be disconnected. If there
+            # is no responce, then the server is down and the user should
+            # be reconnected when the world is back online.
+            #
             @conn{$s}->{reconnect} = 1;
             printf($hb "think ### PING: " . $s . "###\n");
-   
-#            push(@que, { type => "disconnect",
-#                         user => $1,
-#                         pass => $2,
-#                         sock => $s
-#                    }
-#                );
          }
       }
    }
 }
 
-sub online
-{
-   return ($hb eq undef) ? 0 : 1;
-}
-
+#
+# socket_open
+#    Open a connection to the world.
+#
 sub socket_open
 {
    my $new = shift;
 
    my $sock = IO::Socket::INET->new(PeerAddr=>@data{mush_address},blocking=>0);
+#   printf("sock_open: '%s' -> %s\n",$sock,code());
    return $sock;
 }
 
 
+#
+# world_connect
+#    Connect to the world and setup any required structures.
+#    If the world is down, flag the connection to be reconnected.
+#
 sub world_connect
 {
    my ($s,$was_offline) = @_;
@@ -141,27 +213,38 @@ sub world_connect
    @conn{$s}->{client} = $s,
    @conn{$s}->{created} = time() if(!defined @conn{created});
 
-   return 0 if !online();
+   return 0 if !online();                  # hb is offline, don't reconnect
 
-   my $new = socket_open($s);
-   printf($new "\@REMOTEHOSTNAME %s\n",$s->peerhost);
+   my $new = socket_open($s); # connect
    return 0 if($new eq undef);                            # did not connect
 
-   @conn{$s}->{world} = $new;
-   @world{$new} = $s;                                      # reverse lookup
-   $readable->add($new);
+   # optionally send command to tell the world user's address.
+   if(@data{remotehostname_cmd} ne "") {
+      printf($new "@data{remotehostname_cmd} %s\n",$s->peerhost); #
+   }
 
-   if($was_offline) {
+   @conn{$s}->{world} = $new; #                               hook up socket
+   @world{$new} = $s;                                       # reverse lookup
+   $readable->add($new);                                   # add to listener
+
+   if($was_offline) {                                       # reconnect user
       delete @conn{$s}->{disconnect};
 
       if(defined @conn{$s}->{user} && defined @conn{$s}->{pass}) {
          printf($new "connect %s %s\n",@conn{$s}->{user},@conn{$s}->{pass});
+
+         # signal to mush connection info is done
          printf($new "think ### RECONNECT COMPLETE ###\n");
       }
    }
    return 1;
 }
 
+#
+# handle_select
+#    Use select() to look for input from the world or clients and then
+#    process it.
+#
 sub handle_select
 {
    my ($new, $buf,$bytes);
@@ -201,6 +284,10 @@ sub handle_select
    }
 }
 
+#
+# io_world
+#    Input has come in from the world, deal with it.
+#
 sub io_world
 {
    my ($world_sock,$data) = @_;
@@ -218,6 +305,19 @@ sub io_world
    } else {
       my $sock = @conn{$s}->{client};
       printf($sock "%s\n", $data);  # send world output to client
+   }
+
+   if(que_next($sock) eq "connect") {
+      if($data =~ /@data{connect_success}/) {                   # good login
+         my $item = que_pop($sock);                      # pop event off que
+         @conn{$$item{sock}}->{user} = $item->{user};
+         @conn{$$item{sock}}->{pass} = $item->{pass};
+         loggit("%s@%s has connected.",$item->{user},$$item{sock}->peerhost);
+      } elsif($data =~ /@data{connect_fail}/) {
+         que_pop($sock);                           # bad login, don't save it
+      } elsif(que_time($sock) > 4) {                        # event timed out
+         que_pop($sock);
+      }
    }
 }
 
@@ -276,6 +376,11 @@ sub print_var
    return $out;
 }
 
+#
+# user
+#    Given a socket, determine what the user's name is. User needs to
+#    have logged in.
+#
 sub user
 {
    my $sock = id(shift);
@@ -296,16 +401,102 @@ sub smaller
     my $txt = shift;
 
     if($txt =~ /\(0x(.*)\)/) {
-       return $1;
+       return $1 . connected($txt);
     } else {
-       return $txt;
+       return $txt . connected($txt);
     }
 }
 
+#
+# connected
+#    is the socket connected, if so return a 'c' or a '*' for not connected.
+#
+sub connected
+{
+   my $sock = shift;
 
+   return $sock->connected ? "[c]" : "[*]";
+}
+
+#
+# list_sockets
+#    Provide the user with some details on which sockets are connected to
+#    the service.
+#
 sub list_sockets
 {
    my $s = shift;
+   my (%list, %orphan);
+
+   # check listener for sockets
+   for my $sock ($readable->handles) {
+      if(defined @conn{$sock}) {
+         @list{$sock} = {} if !defined @list{$sock};
+         @list{$sock}->{client} = smaller($sock);
+      } elsif(defined @world{$sock}) {
+         if(defined @conn{@world{$sock}}) {
+            @list{@world{$sock}} = {} if !defined @list{@world{$sock}};
+            @list{@world{$sock}}->{world} = smaller($sock);
+         } else {
+            @orphan{$sock} = "world";
+         }
+      } elsif($sock eq $hb) {
+         @list{$sock} = {} if !defined @list{$sock};
+         @list{$sock}->{hb} = smaller($sock);
+      } elsif($sock eq $listener) {
+         @list{$sock} = {} if !defined @list{$sock};
+         @list{$sock}->{listener} = smaller($sock);
+      } else {
+         @orphan{$sock} = "unknown";
+      }
+   }
+
+   for my $key (keys %conn) {
+      if(defined @conn{$key}->{client} &&
+         (!defined @list{$key} || 
+         !defined @list{$key}->{client})) {
+         @list{$key} = {} if not defined @list{$key};
+         @list{$key}->{error} .= "c";
+      }
+      if(defined @conn{$key}->{world}) {
+         if(!defined @list{$key} || !defined @list{$key}->{world}) {
+            @list{$key} = {} if not defined @list{$key};
+            @list{$key}->{error} .= "w"
+         }
+         if(!defined @world{@conn{$key}->{world}}) {
+            @list{$key} = {} if not defined @list{$key};
+            if(!defined @list{$key}->{world}) {
+               @list{$key}->{error} .= "W"
+            } else {
+               @list{$key}->{error} .= "W";
+            }
+         }
+      }
+   }
+
+   for my $key (sort {@list{$a}->{client} cmp @list{$b}->{client}} keys %list){
+      if(defined @list{$key}->{hb}) {
+         printf($s "* %-15s %s\n","hb",@list{$key}->{hb});
+      } elsif(defined @list{$key}->{listener}) {
+         printf($s "* %-15s %s\n","listener",@list{$key}->{listener});
+      } else {
+         printf($s "%-17s %-12s, world %-12s\n",
+                substr(user($key),0,15),
+                @list{$key}->{client},
+                @list{$key}->{world},
+                user($key),
+                defined(@list{$key}->{error}) ? ", err: " . 
+                    @list{$key}->{error} : ""
+               );
+      }
+   }
+
+   for my $key (keys %orphan) {
+      printf($s "orphaned: %s\n",@orphan{$key});
+   }
+
+   return;
+   printf($s "-----------------------\n");
 
    for my $sock ($readable->handles) {
       if(defined @conn{$sock}) {
@@ -317,7 +508,7 @@ sub list_sockets
          if(user($sock) eq "unconnected" && 
             @conn{$sock}->{created} + 300 < time()) {
             printf($s "             *DISCONNECTING*\n");
-            disconnect_client($sock);
+#            disconnect_client($sock);
          }
       } elsif(defined @world{$sock}) {
          printf($s "   world:    %s, status: %-12s, user: %s\n",
@@ -343,7 +534,11 @@ sub list_sockets
    }
 }
 
-
+#
+# io_client
+#    Input has come from the client, echo it to the world and look
+#    for connect commands so they can be stored for reconnecting.
+#
 sub io_client
 {
    my ($s,$data) = @_;
@@ -351,29 +546,78 @@ sub io_client
    return if !defined @conn{$s};
    my $sock = @conn{$s}->{world};
 
-   if(offline()) {
+   if(offline()) {                             # hb is offline, do nothing
       return;
-   } elsif($data =~ /^#\?$/) {
+   } elsif($data =~ /^#\?$/) {                           # internal command
       return list_sockets(@conn{$s}->{client});
    } elsif($data =~ /^\s*connect ([^\;\,\% ]+) ([^\;\,\% ]+)/i) {
       # verify the connect is successfull by having the HB verify the
       # password. The script will need to wait for the responce.
-      
-      push(@que, { type => "connect",
-                   user => $1,
-                   pass => $2,
-                   sock => $s
-                 }
+     
+      que_push($s, { type    => "connect",
+                     created => time(),
+                     user    => $1,
+                     pass    => $2,
+                     sock    => $s
+                   }
           );
-      printf($hb "think ### PASSWORD: [password(*%s,%s)] %s ###\n",
-          $1,$2,@conn{$s}->{client});
    }
    printf($sock "%s\n", $data);
 }
 
-sub next_event
+#
+# que_pop
+#   This really doesn't need to be coded but the que was changed while
+#   the code was running, and this allowed for the que to be stored in
+#   an already existing variable instead of the one i'd perfered.
+#
+sub que_pop
 {
-   return @que[0]->{type} if($#que >= 0);
+   my $sock = shift;
+
+   return if(!defined @data{que} || !defined @data{que}->{$sock});
+   my $que = @data{que}->{$sock};
+   return pop(@$que);
+}
+
+sub que_push
+{
+   my ($sock,$data) = @_;
+
+   @data{que} = {} if(!defined @data{que});
+   @data{que}->{$sock} = [] if !defined @data{que}->{$sock};
+
+   my $que  = @data{que}->{$sock};
+
+   push(@$que,$data);
+}
+
+#
+# que_next
+#    Return the item type of the next que item.
+#
+sub que_next
+{
+   my $sock = shift;
+
+   return if(!defined @data{que} || !defined @data{que}->{$sock});
+   my $que = @data{que}->{$sock};
+   return $$que[0]->{type} if($#$que >= 0);
+}
+
+#
+# que_time
+#    Return the number of seconds since the item was put into the que
+#    for use as a timeout.
+#
+sub que_time
+{
+   my $sock = shift;
+
+   return if(!defined @data{que} || !defined @data{que}->{$sock});
+   my $que = @data{que}->{$sock};
+
+   return time() - $$que[0]->{created} if($#$que >= 0);
 }
 
 sub io_hb
@@ -384,21 +628,9 @@ sub io_hb
    # really disconnecting and not because the world crashed.
    for my $key (keys %conn) {
       if(defined @conn{$key}->{disconnect}) {
+         loggit("%s@%s has disconnected.",
+                user($key),@conn{$key}->{client}->peerhost);
          disconnect_client(@conn{$key}->{client});   # world is up, disconnect
-      }
-   }
-
-   if($data =~ /### PASSWORD: (0|1) ([^ ]+) ###$/) {
-      
-      if(next_event() eq "connect") {
- 
-         my $item = pop(@que);                           # pop event off que
-
-         if($1 eq 1 && $2 eq $$item{sock}) {# successful login, pass was correct
-            @conn{$$item{sock}}->{user} = $item->{user};
-            @conn{$$item{sock}}->{pass} = $item->{pass};
-            loggit("%s@%s has connected.",$item->{user},$$item{sock}->peerhost);
-         }
       }
    }
 }
@@ -513,6 +745,7 @@ sub server_start
 sub hb_open_connect
 {
    if(!online() && @data{hb_next} < time()) {
+#      printf("hb_open_connect: called '%s'\n",code());
       my $prev = ($hb eq undef) ? 0 : 1;
       @data{hb_next} = time() + @data{beat};
       $hb = socket_open();
@@ -620,7 +853,7 @@ sub disconnect_cleanup
       }
       if(user($id) eq "unconnected" && 
          @conn{$id}->{created} + 300 < time()) {
-         disconnect_client(@conn{$id}->{client});
+#         disconnect_client(@conn{$id}->{client});
       }
    }
 }
@@ -643,7 +876,7 @@ sub main
    };
 
    if($@) {
-      printf("ERROR: %s\n",@_);
+      printf("ERROR: %s\n",$@);
    }
 }
 
